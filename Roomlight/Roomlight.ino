66c00b3a0ae14dd738002e9fc6afa8e9c8a67b20
Max Janorschke <programming@max-janorschke.de>
2022-09-18 12:18:36 +0200

    change initial leds

--- Roomlight/Roomlight.ino
+++ Roomlight/Roomlight.ino
@@ -21,15 +21,16 @@ MqttCallbackHandler* mqttCallbackHandler;
 void setup() {
   Serial.begin(9600);
 
-  p_wlan = new WlanESP(WLAN_SSID, WLAN_PASSWORD);
-  p_ota = new OTA_ESP();
-  p_mqtt = new MQTT_ESP(MQTT_SERVER_IP_ADDRESS, MQTT_SERVER_PORT, p_espClient);
-  p_network = new Network(p_wlan, p_ota, p_mqtt);
-
   p_color = new Colors(MODUS_RGB);
   p_keyboardDevice = new Device("deskLightingKeyboard", PIN_D4, 12, 0, 1, 7, p_color);
   p_bedWallDevice = new Device("deskLightingBedWall", PIN_D3, 60, 8, 9, 15, p_color);
   p_bedSideDevice = new Device("deskLightingBedSide", PIN_D2, 60, 16, 17, 23, p_color);
+  animate(); // show lights before connecting
+
+  p_wlan = new WlanESP(WLAN_SSID, WLAN_PASSWORD);
+  p_ota = new OTA_ESP();
+  p_mqtt = new MQTT_ESP(MQTT_SERVER_IP_ADDRESS, MQTT_SERVER_PORT, p_espClient);
+  p_network = new Network(p_wlan, p_ota, p_mqtt);
 
   mqttCallbackHandler = new MqttCallbackHandler(
     p_mqtt,
@@ -48,11 +49,14 @@ void setup() {
 }
 
 void loop() {
+  animate();
+  p_network->loop();
+}
+
+void animate() {
   p_keyboardDevice->animate();
   p_bedWallDevice->animate();
   p_bedSideDevice->animate();
-  
-  p_network->loop();
 }
 
 void onMqttPayload(char* pc_topic, u_int8_t* pi_payload, unsigned int i_length) {
